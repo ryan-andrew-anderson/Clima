13f2b7c86415de329710d0e91b911524901b1388
Ryan A. Anderson <ryan.anderson9921@stu.mtec.edu>
2021-12-19 20:38:15 -0700

    Setting up URLSession and task

---
+++ Clima/Model/WaetherData.swift
@@ -0,0 +1,24 @@
+//
+//  WaetherData.swift
+//  Clima
+//
+//  Created by Ryan Anderson on 12/19/21.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Decodable{
+    
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Decodable {
+    let temp: Double
+}
+
+struct Weather: Decodable {
+    let description: String
+}
--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -9,12 +9,45 @@
 import Foundation
 
 struct WeatherManager {
-    let weatherURL = "http://api.openweathermap.org/data/2.5/weather?appid=6dbc259a546e8488c9d786703b615931&units=imperial"
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=6dbc259a546e8488c9d786703b615931&units=imperial"
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherURL)&q=\(cityName)"
+        performRequest(urlString: urlString)
         print(urlString)
     }
     
-    
-
+    func performRequest(urlString: String) {
+        //1.Create URL
+        if let url = URL(string: urlString) {
+            //2.Create a URLSession
+            let session = URLSession(configuration: .default)
+            //3.Give the session a task
+            let task = session.dataTask(with: url) { data, response, error in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                if let safeData = data {
+                    self.parseJSON(weather: safeData)
+                }
+            }
+            //4.Start the Task
+            task.resume()
+        }
+    }
+    /// Turns JSON data with REST API  into a swift object
+    func parseJSON(weather: Data) {
+        // Declare JSON decoder
+        let decoder = JSONDecoder()
+        // use do block to "catch" any error's
+        do {
+            //creating data object
+            let decodedData = try decoder.decode(WeatherData.self, from: weather)
+            //printing data object
+            print(decodedData.weather.description)
+        }catch {
+            //if error pops up, it prints
+            print(error)
+        }
+    }
 }
